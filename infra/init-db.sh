03ec685e5a6417902a1cf7b7fef3a198f651b900
Paulo Leite <foxmodem@hotmail.com>
2026-07-13 18:54:12 -0300

    fix: correct parent pom inheritance and clean up scaffolding

---
+++ infra/init-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE orders;
+    CREATE DATABASE payments;
+    GRANT ALL PRIVILEGES ON DATABASE orders TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE payments TO $POSTGRES_USER;
+EOSQL
